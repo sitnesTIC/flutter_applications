91c336a1ca4fa7e9d98523e1b8f6948481ac5834
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-14 18:34:06 +0100

    Separando la lógica en varios archivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_applications/screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(
-        child: Text('Hola món!!'),
-      ),
+      debugShowCheckedModeBanner: true,
+      home: HomeScreen(),
     );
   }
 }
