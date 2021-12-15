e6941a607c9752c502c7353be0c927ad51f988b8
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-15 11:58:55 +0100

    BuildContext y Scaffold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('HomeScreen'),
+    return const Scaffold(
+      body: Center(
+        child: Text('HomeScreen'),
+      ),
     );
   }
 }
