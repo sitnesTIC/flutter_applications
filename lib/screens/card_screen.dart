7909ee4aa0dbdbbe8c27c17397dab9ca90aa02f2
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-21 14:12:01 +0100

    Card Widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_applications/themes/app_theme.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Card Screen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            Card(
+              child: Column(
+                children: const [
+                  ListTile(
+                    leading: Icon(
+                      Icons.photo_album_outlined,
+                      color: AppTheme.primary,
+                    ),
+                    title: Text('Sóc un títol'),
+                    subtitle: Text(
+                        'please dirt cook seat canal yesterday worse mostly shaking ordinary machine where organized shore gather pole hold example parallel grass height pain turn winter'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
