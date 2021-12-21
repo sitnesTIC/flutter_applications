2992e9856d1183f17bfd13cd49786f9a6c93f0e8
sitnesTIC <sitnes.tic@andorra.ad>
2021-12-21 10:47:17 +0100

    Header Cuadrado

---
+++ lib/src/screens/headers_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_applications/src/widgets/headers_widget.dart';
+
+class HeadersScreen extends StatelessWidget {
+  const HeadersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: HeaderCuadrado());
+  }
+}
---
+++ lib/src/widgets/headers_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615AAB),
+    );
+  }
+}
