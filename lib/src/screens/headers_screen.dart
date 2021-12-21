45db4a9aee6de81bd55fd2411f6e23c752b40703
sitnesTIC <sitnes.tic@andorra.ad>
2021-12-21 11:25:31 +0100

    Header Diagonal

--- lib/src/screens/headers_screen.dart
+++ lib/src/screens/headers_screen.dart
@@ -7,6 +7,7 @@ class HeadersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return const Scaffold(body: HeaderCuadrado());
-    return const Scaffold(body: HeaderBordesRedondeados());
+    // return const Scaffold(body: HeaderBordesRedondeados());
+    return const Scaffold(body: HeaderDiagonal());
   }
 }
--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -25,3 +25,43 @@ class HeaderBordesRedondeados extends StatelessWidget {
             )));
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      // color: const Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final llapis = Paint();
+    // propietats
+    llapis.color = const Color(0xff615AAB);
+    llapis.style = PaintingStyle.fill; // .stroke
+    llapis.strokeWidth = 5;
+    final tras = Path();
+    // dibuixar el traç amb el llapis
+    tras.moveTo(0, size.height * 0.35);
+    tras.lineTo(size.width, size.height * 0.30);
+    tras.lineTo(size.width, 0);
+    tras.lineTo(0, 0);
+    tras.lineTo(0, size.height * 0.5);
+    canvas.drawPath(tras, llapis);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
