9acddb837722959b264d44feeb798db248128fa2
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-21 16:48:17 +0100

    TextFormFields

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,8 +1,9 @@
 export 'package:flutter_applications/screens/alert_screen.dart';
 export 'package:flutter_applications/screens/alert_screen.dart';
+export 'package:flutter_applications/screens/animated_screen.dart';
 export 'package:flutter_applications/screens/avatar_screen.dart';
 export 'package:flutter_applications/screens/card_screen.dart';
 export 'package:flutter_applications/screens/home_screen.dart';
+export 'package:flutter_applications/screens/inputs_screen.dart';
 export 'package:flutter_applications/screens/listview1_screen.dart';
 export 'package:flutter_applications/screens/listview2_screen.dart';
-export 'package:flutter_applications/screens/animated_screen.dart';
