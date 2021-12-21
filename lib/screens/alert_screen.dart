5c84dc9269472e19aa6382378228532037bca6d9
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-21 15:51:03 +0100

    Mostrar una alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,6 +3,37 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => AlertDialog(
+              elevation: 5,
+              title: const Text('Títol'),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text(
+                      'cannot proud tie highest apart ourselves engine parts audience principle army'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FlutterLogo(
+                    size: 75,
+                  )
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel'),
+                )
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,14 +51,12 @@ class AlertScreen extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
           ),
-          onPressed: () {},
+          onPressed: () => displayDialog(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
