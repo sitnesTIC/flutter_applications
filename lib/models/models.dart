c436aa553ddb77cc56e0b1c6f0031b91fd183848
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-21 13:22:22 +0100

    Modelo y Listado de Rutas

---
+++ lib/models/models.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_applications/models/menu_option.dart';
--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 // -> GS
+import 'package:flutter_applications/models/models.dart';
 import 'package:flutter_applications/screens/screens.dart';
 // <- GS
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    // TODO: borrar home
+    MenuOption(
+        route: 'home',
+        name: 'Home Screen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOption(
+        route: 'listview1',
+        name: 'Listview1 Screen',
+        screen: const Listview1Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'listview2',
+        name: 'Listview2 Screen',
+        screen: const Listview2Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'alert',
+        name: 'Alertes - Alerts',
+        screen: const AlertScreen(),
+        icon: Icons.add_a_photo_outlined),
+    MenuOption(
+        route: 'card',
+        name: 'Tergetes - Cards',
+        screen: const CardScreen(),
+        icon: Icons.credit_card),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
