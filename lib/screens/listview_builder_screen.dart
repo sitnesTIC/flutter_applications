2fe29a63a83aa34c8afce36eb2477a3f143007eb
sitnesTICand <sitnes.tic@andorra.ad>
2021-12-23 16:14:55 +0100

    InfiniteScroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 
-class ListviewBuilderScreen extends StatelessWidget {
+class ListviewBuilderScreen extends StatefulWidget {
   const ListviewBuilderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListviewBuilderScreen> createState() => _ListviewBuilderScreenState();
+}
+
+class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
+
+  void add5() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +35,9 @@ class ListviewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
                 width: double.infinity,
@@ -19,7 +45,7 @@ class ListviewBuilderScreen extends StatelessWidget {
                 fit: BoxFit.cover,
                 placeholder: const AssetImage('assets/jar-loading.gif'),
                 image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${index + 1}'));
+                    'https://picsum.photos/500/300?image=${imagesIds[index]}'));
           },
         ),
       ),
