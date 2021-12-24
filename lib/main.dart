4833734f52aacb567f185dc648e6aecb246136a9
sitnesTIC <sitnes.tic@andorra.ad>
2021-12-24 13:56:48 +0100

    Shared Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_applications/shared_preferences/preferences.dart';
+
 import 'package:flutter_applications/screens/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Preferences.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -17,6 +23,7 @@ class MyApp extends StatelessWidget {
         HomeScreen.routeName: (_) => const HomeScreen(),
         SettingsScreen.routeName: (_) => const SettingsScreen(),
       },
+      theme: ThemeData.light(),
     );
   }
 }
