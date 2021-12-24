c0839e4d128c7e6f0af0fbdcaf58ae46462dd706
sitnesTIC <sitnes.tic@andorra.ad>
2021-12-24 13:43:17 +0100

    Hacer funcionar los Switches y Radios

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_applications/widgets/widgets.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static const String routeName = 'Settings';
   const SettingsScreen({Key? key}) : super(key: key);
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkmode = false;
+  int gender = 1;
+  String name = 'Pep';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,29 +32,42 @@ class SettingsScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 45, fontWeight: FontWeight.w300)),
               const Divider(),
               SwitchListTile.adaptive(
-                value: true,
+                value: isDarkmode,
                 title: const Text('Darkmode'),
-                onChanged: (value) {},
+                onChanged: (value) {
+                  isDarkmode = value;
+                  setState(() {});
+                },
               ),
               const Divider(),
               RadioListTile<int>(
                 value: 1,
-                groupValue: 1,
+                groupValue: gender,
                 title: const Text('Masculí'),
-                onChanged: (value) {},
+                onChanged: (value) {
+                  gender = value ?? 1;
+                  setState(() {});
+                },
               ),
               const Divider(),
               RadioListTile<int>(
                 value: 2,
-                groupValue: 1,
+                groupValue: gender,
                 title: const Text('Femení'),
-                onChanged: (value) {},
+                onChanged: (value) {
+                  gender = value ?? 2;
+                  setState(() {});
+                },
               ),
               const Divider(),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: TextFormField(
                   initialValue: 'Gerard',
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
                   decoration: const InputDecoration(
                       labelText: 'Nom', helperText: 'Nom de l\'usuari'),
                 ),
